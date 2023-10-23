e7cf1cdb3bb81fa4d3abb610e53e7d7ee9159458
Pasha Fentisov <pasha16.ua@gmail.com>
2023-10-24 00:45:55 +0300

    refactored code, extracted methods, fixed primitive fields filtration

--- src/main/java/com/pashonokk/genericquery/api/GenericSpecification.java
+++ src/main/java/com/pashonokk/genericquery/api/GenericSpecification.java
@@ -1,5 +1,6 @@
 package com.pashonokk.genericquery.api;
 
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
@@ -24,56 +25,19 @@ public class GenericSpecification<T> implements Specification<T> {
     @Override
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
         List<Predicate> predicates = new ArrayList<>();
-        List<String> primitiveFields = Arrays.stream(root.getModel().getJavaType().getDeclaredFields())
-                .filter(field -> isPrimitive(field.getType()))
-                .map(Field::getName).toList();
+        List<String> primitiveFields = getNamesOfPrimitiveFields(root);
 
-        List<String> listFields = Arrays.stream(root.getModel().getJavaType().getDeclaredFields())
-                .filter(field -> field.isAnnotationPresent(OneToMany.class)) //todo implement ManyToMany
-                .map(Field::getName).toList();
+        List<String> listFields = getNamesOfListFields(root);
 
         for (Criteria criteria : searchCriteria.getCriteriaList()) {
-            if (primitiveFields.contains(criteria.getKey())) {  //1
-                if (root.get(criteria.getKey()).getJavaType() == String.class) {
-                    List<String> values = criteria.getValues();
-                    List<Predicate> orPredicates = new ArrayList<>();
-
-                    for (String value : values) {
-                        orPredicates.add(builder.like(root.get(criteria.getKey()), "%" + value + "%"));
-                    }
-
-                    predicates.add(builder.or(orPredicates.toArray(new Predicate[0])));
-                } else {
-                    predicates.add(builder.equal(root.get(criteria.getKey()), criteria.getValues().get(0)));
-                }
-                return builder.and(predicates.toArray(new Predicate[0]));
+            if (primitiveFields.contains(criteria.getKey())) {
+                filterByPrimitiveFields(root, builder, criteria, predicates);
             } else if (listFields.contains(criteria.getKey().split("\\.")[0])) {//2
-                List<Field> fields = Arrays.stream(root.getModel().getJavaType().getDeclaredFields())
-                        .filter(field -> field.isAnnotationPresent(OneToMany.class)).toList();
-                Type genericType = fields.get(0).getGenericType();
-                Class<?> genericClass = null;
-                if (genericType instanceof ParameterizedType) {
-                    ParameterizedType parameterizedType = (ParameterizedType) genericType;
-                    Type[] typeArguments = parameterizedType.getActualTypeArguments();
-
-                    if (typeArguments.length > 0) {
-                        genericClass = (Class<?>) typeArguments[0];
-                        System.out.println("Class of List Generic Type: " + genericClass.getName());
-                    }
-                }
-
-                Subquery subquery = query.subquery(genericClass);
-                Root subRoot = subquery.from(genericClass);
-
-                subquery.select(subRoot);
-                subquery.where(subRoot.get("client")
-                                .in(root),
-                        subRoot.get("description") //todo
-                                .in(criteria.getValues()));
-
+                Subquery subquery = filterByListFields(root, query, criteria);
                 return builder.exists(subquery);
-
             } else {
+                //todo можем шукати по одному значенню і не можем шукати по декільком полям
+                //todo варто все класти в загальні predicates а не повертати в кожній конструкції щось своє
                 Path<String> path = getPath(root, criteria.getKey());
                 if (path.getJavaType() == String.class) {
                     return builder.like(
@@ -83,10 +47,62 @@ public class GenericSpecification<T> implements Specification<T> {
                 }
             }
         }
-        return null;
+        return builder.and(predicates.toArray(new Predicate[0]));
     }
 
-    //todo now support only one field for filterng
+    private List<String> getNamesOfListFields(Root<T> root) {
+        return Arrays.stream(root.getModel().getJavaType().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(OneToMany.class) || field.isAnnotationPresent(ManyToMany.class))
+                .map(Field::getName).toList();
+    }
+
+    private List<String> getNamesOfPrimitiveFields(Root<T> root) {
+        return Arrays.stream(root.getModel().getJavaType().getDeclaredFields())
+                .filter(field -> isPrimitive(field.getType()))
+                .map(Field::getName).toList();
+    }
+
+    private Subquery filterByListFields(Root<T> root, CriteriaQuery<?> query, Criteria criteria) {
+        List<Field> fields = Arrays.stream(root.getModel().getJavaType().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(OneToMany.class) || field.isAnnotationPresent(ManyToMany.class))
+                .toList();
+        //todo тут має бути цикл бо поки робим тільки для одного філда
+        //todo не можем юзати багато значеннь
+        Type genericType = fields.get(0).getGenericType();
+        Class<?> genericClass = null;
+        ParameterizedType parameterizedType = (ParameterizedType) genericType;
+        Type[] typeArguments = parameterizedType.getActualTypeArguments();
+
+        if (typeArguments.length > 0) {
+            genericClass = (Class<?>) typeArguments[0];
+        }
+
+        Subquery subquery = query.subquery(genericClass);
+        Root subRoot = subquery.from(genericClass);
+
+        subquery.select(subRoot);
+
+        subquery.where(subRoot.get("client")
+                        .in(root),
+                subRoot.get(criteria.getKey().split("\\.")[1])
+                        .in(criteria.getValues()));
+        return subquery;
+    }
+
+    private void filterByPrimitiveFields(Root<T> root, CriteriaBuilder builder, Criteria criteria, List<Predicate> predicates) {
+        if (root.get(criteria.getKey()).getJavaType() == String.class) {
+            List<String> values = criteria.getValues();
+            List<Predicate> orPredicates = new ArrayList<>();
+
+            for (String value : values) {
+                orPredicates.add(builder.like(root.get(criteria.getKey()), "%" + value + "%"));
+            }
+
+            predicates.add(builder.or(orPredicates.toArray(new Predicate[0])));
+        } else {
+            predicates.add(builder.equal(root.get(criteria.getKey()), criteria.getValues().get(0)));
+        }
+    }
     private Path<String> getPath(Root<T> root, String key) {
         String[] fields = key.split("\\.");
         Path<String> path = root.get(fields[0]);
@@ -95,7 +111,6 @@ public class GenericSpecification<T> implements Specification<T> {
         }
         return path;
     }
-
 
     public static boolean isPrimitive(Class<?> type) {
         return type.isPrimitive() ||
